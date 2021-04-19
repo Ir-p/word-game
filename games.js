020e056de538fd68855b82f3e7ce166911b44cf3
Irina Pyak <ir.pyak@gmail.com>
2021-04-18 23:21:58 -0400

    to add index.html and games.js files

---
+++ games.js
@@ -0,0 +1,26 @@
+//Add words array storage
+
+// Add click EventListener to Start button; to call Start game function
+
+// Add click EventListener to Reset button; to call reset game function
+
+// Add keydown EventListener to keydown document; 
+
+
+// Start game
+
+    //start the timer -> startTimer
+    //show the blanks -> show blanks
+
+// Start timer
+
+    //use setInterval to call the function
+        //check if time is done
+            // call "game over" function
+        // else 
+            // check if the word is complete
+                // call "game won"
+            // or else deduct the time count
+
+// show blanks (game round)
+    
