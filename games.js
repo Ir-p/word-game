dea1c903482f3ea22a55399775876771cbbb631d
Irina Pyak <ir.pyak@gmail.com>
2021-04-21 21:37:23 -0400

    to add pseudo code and start button working with a tutor

--- games.js
+++ games.js
@@ -1,14 +1,19 @@
+var startButton = document.querySelector(".start-button")
+console.log(startButton)
+
 //Add words array storage
-
+var wordArray = ["object", 'function', "array", "javaScript"]
 // Add click EventListener to Start button; to call Start game function
-
+startButton.addEventListener('click', startGame)
 // Add click EventListener to Reset button; to call reset game function
 
 // Add keydown EventListener to keydown document; 
 
 
 // Start game
-
+ function startGame() {
+    console.log('start game')
+ }
     //start the timer -> startTimer
     //show the blanks -> show blanks
 
@@ -23,4 +28,7 @@
             // or else deduct the time count
 
 // show blanks (game round)
+
+// option 1 game won, alert they won, add to the win counter, re-pop the start game button to play again
+// option 2 game lost, alert they lost, add the lose counter, re-pop the start game button to play again
     
